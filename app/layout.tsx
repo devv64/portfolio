b0d4455598e6c2c0a85e8366eb54f8ab8cb74e1a
Dev <devextraemail@gmail.com>
2023-10-12 23:35:59 -0400

    Full landing screen completed and animated

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import Header from '@/components/header'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  // TODO: Change favicon
+  title: 'Dev Patel.',
+  description: 'Dev Patel is a software engineer based in Hoboken, New Jersey.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
+        <div className="-z-10 bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="-z-10 bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
+}
