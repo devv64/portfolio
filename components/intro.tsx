f2068d03a1899925da600e191b63b5777a2cfcd8
Dev <devextraemail@gmail.com>
2024-06-11 21:18:49 -0400

    updated info for hero and contact sections

--- components/intro.tsx
+++ components/intro.tsx
@@ -57,7 +57,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         >
-            <span className="font-bold">Hello, I'm Ricardo.</span> I'm a <span className="font-bold">full-stack developer</span> with <span className="font-bold">8 years</span> of experience. I enjoy building <span className="italic">sites & apps.</span> My focus is <span className="underline">React (Next.js)</span>.
+            <span className="font-bold">Hello, I'm Dev.</span> I'm a <span className="font-bold">software engineer</span> currently pursuing a <span className="font-bold"> masters degree</span> in computer science at <span className="font-bold">Cornell University</span>. I enjoy solving problems, primarily with <span className="underline">software</span>.
         </motion.h1>
 
         <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
